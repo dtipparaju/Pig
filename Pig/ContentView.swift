d77003e95c0ee3e65d7ebebd09a4547b307c0c3f
Dhanush Tipparaju <dhanushtips@gmail.com>
2022-11-09 10:09:47 -0600

    added game functionality

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -28,16 +28,46 @@ struct ContentView: View {
                 CustomText(text: "Turn Score: \(turnScore)")
                 HStack {
                     Button("Roll") {
-                        
+                        chooseRandom(times: 3)
+                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                     Button("Hold"){
-                        
+                        gameScore += turnScore
+                        endTurn()
+                        withAnimation(.easeInOut(duration: 1)){
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                 }
                 CustomText(text: "Game Score: \(gameScore)")
             }
+        }
+    }
+    
+    func endTurn() {
+        turnScore = 0
+        randomValue = 0
+    }
+    
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                endTurn()
+            }
+        }
+        if times == 0 {
+            if randomValue == 1 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    endTurn()
+                }
+            }
+        }
+        else {
+            turnScore += randomValue
         }
     }
 }
